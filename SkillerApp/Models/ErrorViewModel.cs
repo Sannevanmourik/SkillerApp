3d6cec5b49f286333a23b0847b762af10bbb6bba
Sanne <sanne.van.mourik@capgemini.com>
2018-09-04 14:35:44 +0200

    Add project files.

---
+++ SkillerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SkillerApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
